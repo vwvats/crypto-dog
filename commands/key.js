981f2835a80d0a9ce180d50c28f920515f8504a8
VW Vats <vwvats@gmail.com>
2022-05-02 22:54:39 +0530

    set, show, and delete key commands

--- commands/key.js
+++ commands/key.js
@@ -1,13 +1,46 @@
-const key = {
-  set() {
-    console.log('hello from set')
-  },
-  show() {
-    console.log('hello from show')
-  },
-  remove() {
-    console.log('hello from remove')
-  },
-}
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+const { isRequired } = require('../utils/validation');
 
-module.exports = key
+const key = {
+  async set() {
+    const keyManager = new KeyManager();
+    const input = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'key',
+        message: 'Enter API Key '.blue + 'https://nomics.com/',
+        validate: isRequired
+      }
+    ]);
+    const key = keyManager.setKey(input.key);
+    if (key) {
+      console.log('API Key Set'.green);
+    }
+  },
+
+  show() {
+    try {
+      const keyManager = new KeyManager();
+      const key = keyManager.getKey();
+      console.log('Current API Key: ', key.yellow);
+      return key;
+    } catch (err) {
+      console.error(err.message.red);
+    }
+  },
+
+  remove() {
+    try {
+      const keyManager = new KeyManager();
+      keyManager.deleteKey();
+      console.log('Key Removed'.blue);
+      return;
+    } catch (err) {
+      console.error(err.message.red);
+    }
+  }
+};
+
+module.exports = key;
