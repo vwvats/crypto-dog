4e52f7919feda57bb4679ff63b48f1578081c048
VW Vats <vwvats@gmail.com>
2022-05-02 22:13:32 +0530

    initialized project, installed dependencies

---
+++ bin/cdog.js
@@ -0,0 +1,9 @@
+#!usr/bin/env node
+const program = require('commander')
+const pkg = require('../package.json')
+
+program
+  .version(pkg.version)
+  .parse(process.argv)
+console.log("hello from crypto dog")
+
